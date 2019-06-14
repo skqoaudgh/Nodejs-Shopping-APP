cff099aaff1a386a75a083b693aa10097c6b7f0d
myungho <skqoaudgh2@hanmail.net>
2019-06-14 18:20:36 +0900

    Stripe Card Elements 추가

---
+++ public/javascripts/checkout.js
@@ -0,0 +1,61 @@
+var stripe = Stripe('pk_test_1LEAer1QfhJ6v5nBE5lf4OPz00pPqoZfpZ');
+var elements = stripe.elements();
+
+var style = {
+    base: {
+      color: '#32325d',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
+    }
+};
+
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('charge-error');
+    if (event.error) {
+      displayError.textContent = event.error.message;
+    } else {
+      displayError.textContent = '';
+    }
+});
+
+var form = document.getElementById('checkout-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+        if (result.error) {
+            // Inform the customer that there was an error.
+            var errorElement = document.getElementById('charge-error');
+            errorElement.textContent = result.error.message;
+        } else {
+            // Send the token to your server.
+            stripeTokenHandler(result.token);
+        }
+    });
+});
+
+function stripeTokenHandler(token) {
+    console.log('1');
+    
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById('checkout-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+  
+    // Submit the form
+    form.submit();
+}
